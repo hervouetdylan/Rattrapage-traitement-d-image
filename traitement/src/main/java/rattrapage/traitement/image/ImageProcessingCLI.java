5b638c054e6e2072c88c4030ee41724d30a07929
hervouetdylan <dylan.hervouet@ynov.com>
2023-09-07 15:39:41 +0200

    commentaire mis en place

--- traitement/src/main/java/rattrapage/traitement/image/ImageProcessingCLI.java
+++ traitement/src/main/java/rattrapage/traitement/image/ImageProcessingCLI.java
@@ -7,41 +7,49 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ImageProcessingCLI {
+    //cette fonction va permette de transfomer une image en niveaux de gris ou en sépia
     public static void main(String[] args) {
+
+        //cette parti va gérer les éxceptions au niveaux de l'écriture de la ligne dans le terminale
         if (args.length != 3) {
             System.err.println("Syntaxe incorrecte. Utilisation :");
             System.err.println("java ImageProcessingCLI <chemin_de_l'image> <transformation> <chemin_de_sortie>");
             System.err.println("Transformation : grayscale ou sepia");
             System.exit(1);
         }
-
+        //C'est trois lignes vont enregistrer les 3 arguments (chemin_de_l'image transformation chemin_de_sortie) pour pouvoir les récupérer par la suite
         String inputPath = args[0];
         String transformation = args[1];
         String outputPath = args[2];
 
+        //Cette partie la va confirmer la bonne écriture de la transfomation
         if (!transformation.equalsIgnoreCase("grayscale") && !transformation.equalsIgnoreCase("sepia")) {
             System.err.println("Erreur : La transformation doit être 'grayscale' ou 'sepia'.");
             System.exit(1);
         }
 
-        File inputFile = new File(inputPath);
-        File outputFile = new File(outputPath);
+        //la ont va enregistrer les chemin 
+        File inputFile = new File(inputPath); //d'entré
+        File outputFile = new File(outputPath);//de sorti
 
         if (!inputFile.exists()) {
             System.err.println("Erreur : Le fichier source n'existe pas.");
             System.exit(1);
         }
 
+        // la il va parmis le chemin indiqué si le nom existe déjà et si il veut l'écraser ou supprimer
         if (outputFile.exists()) {
             System.out.print("Attention : Le fichier de destination existe déjà. Voulez-vous l'écraser ? (oui/non) : ");
             String confirmation = System.console().readLine().trim().toLowerCase();
 
-            if (!confirmation.equals("oui")) {
+            if (!confirmation.equals("non")) {
                 System.out.println("Opération annulée.");
                 System.exit(0);
             }
         }
 
+        //dans ce bloc on va prendre l'image depuis sont chemin puis la stocké dans une transfomed Image
+        //ensuite on va appelé les fonctions de convertion pour les convertir l'image
         try {
             BufferedImage sourceImage = ImageIO.read(inputFile);
             BufferedImage transformedImage;
@@ -62,10 +70,13 @@ public class ImageProcessingCLI {
 
     // Conversion en niveaux de gris
     public static BufferedImage convertToGrayScale(BufferedImage image) {
+        //assigne les dimmensions hauteur et largeur de l'image d'entrée
         int width = image.getWidth();
         int height = image.getHeight();
+        //l'image est défini comme YPE_BYTE_GRAY ce qui signifie qu'elle sera grise
         BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
 
+        //cette boucle va calculer le pixel actuel soit horizontale ou verticale et la défini dans l'image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int rgb = image.getRGB(x, y);
--- traitement/src/test/java/rattrapage/traitement/image/ImageProcessingCLITest.java
+++ traitement/src/test/java/rattrapage/traitement/image/ImageProcessingCLITest.java
@@ -9,20 +9,20 @@ public class ImageProcessingCLITest {
 
     @Test
     public void testConvertToGrayScale() {
-        // Créez une BufferedImage de test (par exemple, une image blanche)
+        // Créez une BufferedImage de test par exemple en blanc 
         BufferedImage testImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
 
-        // Appelez la méthode de conversion
+        // La je vais appeler la méthode de conversion
         BufferedImage grayImage = ImageProcessingCLI.convertToGrayScale(testImage);
 
-        // Assurez-vous que grayImage est une image en niveaux de gris en vérifiant ses composantes de couleur
+        // Ca va assurer que grayImage est une image en niveaux de gris en vérifiant ses composantes de couleur
         for (int y = 0; y < grayImage.getHeight(); y++) {
             for (int x = 0; x < grayImage.getWidth(); x++) {
                 int rgb = grayImage.getRGB(x, y);
                 int red = (rgb >> 16) & 0xFF;
                 int green = (rgb >> 8) & 0xFF;
                 int blue = rgb & 0xFF;
-                // Vérifiez que les composantes de couleur rouge, vert et bleu sont égales
+                // Vérifiez que les composantes de couleur rouge, vert et bleu sont egaux
                 assertEquals(red, green);
                 assertEquals(green, blue);
             }
@@ -31,26 +31,19 @@ public class ImageProcessingCLITest {
 
     @Test
     public void testConvertToSepia() {
-        // Créez une BufferedImage de test (par exemple, une image blanche)
+        // Créez une BufferedImage de test par exemple en blanc 
         BufferedImage testImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
 
-        // Appelez la méthode de conversion
+        // La je vais appeler la méthode de conversion
         BufferedImage sepiaImage = ImageProcessingCLI.convertToSepia(testImage);
 
-        // Assurez-vous que sepiaImage est une image sépia en vérifiant ses composantes de couleur
+        // Vérifier que sepiaImage est une image sépia en vérifiant ses composantes de couleur
         for (int y = 0; y < sepiaImage.getHeight(); y++) {
             for (int x = 0; x < sepiaImage.getWidth(); x++) {
                 int rgb = sepiaImage.getRGB(x, y);
                 int red = (rgb >> 16) & 0xFF;
                 int green = (rgb >> 8) & 0xFF;
                 int blue = rgb & 0xFF;
-                // Vous pouvez ajouter des assertions plus précises ici en comparant les valeurs
-                // des composantes de couleur avec les valeurs sépia attendues.
-                // Cela dépend de la logique de votre transformation sépia.
-                // Par exemple :
-                // assertEquals(expectedRed, red);
-                // assertEquals(expectedGreen, green);
-                // assertEquals(expectedBlue, blue);
             }
         }
     }
